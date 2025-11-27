bbde019f76b56d107ec1c48913c236844ad44495
tess1o <kerriden1@gmail.com>
2025-11-27 18:10:24 +0200

    Fixed debug import for native

--- backend/src/main/java/org/github/tess1o/geopulse/importdata/ImportDataNativeConfig.java
+++ backend/src/main/java/org/github/tess1o/geopulse/importdata/ImportDataNativeConfig.java
@@ -1,16 +1,14 @@
 package org.github.tess1o.geopulse.importdata;
 
 import io.quarkus.runtime.annotations.RegisterForReflection;
-import org.github.tess1o.geopulse.importdata.model.ImportJob;
-import org.github.tess1o.geopulse.importdata.model.ImportJobResponse;
-import org.github.tess1o.geopulse.importdata.model.ImportOptions;
-import org.github.tess1o.geopulse.importdata.model.ImportStatus;
+import org.github.tess1o.geopulse.importdata.model.*;
 
 @RegisterForReflection(targets = {
         ImportStatus.class,
         ImportJob.class,
         ImportJobResponse.class,
         ImportOptions.class,
+        DebugImportRequest.class,
 })
 public class ImportDataNativeConfig {
 }
