9c398da9fb7e7f003e2f6dbd24179f5d2b479783
tess1o <kerriden1@gmail.com>
2025-11-27 22:43:12 +0200

    Fixed corrupted files and fixed version updater script

--- update_version.sh
+++ update_version.sh
@@ -31,7 +31,52 @@ files_to_update=(
 
 for file in "${files_to_update[@]}"; do
     if [ -f "$file" ]; then
-        sed -i '' "s/$CURRENT_VERSION/$NEW_VERSION/g" "$file"
+        case "$file" in
+            *.xml)
+                # For XML files: only replace version within <version> tags
+                sed -i '' "s|<version>$CURRENT_VERSION</version>|<version>$NEW_VERSION</version>|g" "$file"
+                ;;
+            *.yaml)
+                # For YAML files: replace version values but not IP addresses
+                # Match patterns like: "version: 1.8.0", "tag: 1.8.0", "appVersion: \"1.8.0\""
+                sed -i '' -E \
+                    -e "s/(version: )$CURRENT_VERSION/\1$NEW_VERSION/g" \
+                    -e "s/(tag: )$CURRENT_VERSION/\1$NEW_VERSION/g" \
+                    -e "s/(appVersion: \")$CURRENT_VERSION(\")/\1$NEW_VERSION\2/g" \
+                    "$file"
+                ;;
+            *.md)
+                # For Markdown: replace version references but avoid IPs
+                # Match patterns like: ":1.8.0", "tag/1.8.0", "/1.8.0/"
+                sed -i '' -E \
+                    -e "s/(:)$CURRENT_VERSION/\1$NEW_VERSION/g" \
+                    -e "s/(\\/)$CURRENT_VERSION(\\\/)/\1$NEW_VERSION\2/g" \
+                    -e "s/(tag\\/)$CURRENT_VERSION/\1$NEW_VERSION/g" \
+                    -e "s/(version )$CURRENT_VERSION/\1$NEW_VERSION/g" \
+                    "$file"
+                ;;
+            .env.example)
+                # For .env files: only replace GEOPULSE_VERSION value
+                sed -i '' "s|^GEOPULSE_VERSION=$CURRENT_VERSION|GEOPULSE_VERSION=$NEW_VERSION|g" "$file"
+                ;;
+            *.properties)
+                # For properties files: replace application.version property
+                sed -i '' "s|^application\.version=$CURRENT_VERSION|application.version=$NEW_VERSION|g" "$file"
+                ;;
+            *.json)
+                # For JSON files: replace version in "version": "x.y.z" pattern
+                sed -i '' -E "s/(\"version\": \")$CURRENT_VERSION(\")/\1$NEW_VERSION\2/g" "$file"
+                ;;
+            *.conf)
+                # For nginx.conf: replace in specific max-age patterns
+                sed -i '' -E "s/(max-age=)$CURRENT_VERSION/\1$NEW_VERSION/g" "$file"
+                ;;
+            *)
+                # Default: use careful replacement avoiding IP addresses
+                # Only replace if preceded by common version delimiters
+                sed -i '' -E "s/([:=\/\"' ])$CURRENT_VERSION([\"' \/])/\1$NEW_VERSION\2/g" "$file"
+                ;;
+        esac
         echo "Updated $file"
     else
         echo "Warning: $file not found."
