c1bf821f278a738429e71ac3b85008e8c97c65cf
tess1o <kerriden1@gmail.com>
2026-02-19 00:02:35 +0200

    Fixed Test Connection for Immich v2 server.

--- backend/src/main/java/org/github/tess1o/geopulse/immich/model/ImmichSearchRequest.java
+++ backend/src/main/java/org/github/tess1o/geopulse/immich/model/ImmichSearchRequest.java
@@ -1,11 +1,13 @@
 package org.github.tess1o.geopulse.immich.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ImmichSearchRequest {
     private String takenAfter;
     private String takenBefore;
